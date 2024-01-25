c044d7518f34d84b653d3b374547fbf4a81a4201
freddyrosa16 <freddy.rosa16@gmail.com>
2024-01-25 16:10:41 -0400

    4. Who are you?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,12 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
+import sys
+
+sys.path.append('/path/to/hidden_4')
+
 if __name__ == "__main__":
     import hidden_4
-    for name in dir(hidden_4):
-        if name[0] != "_":
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
             print(name)
