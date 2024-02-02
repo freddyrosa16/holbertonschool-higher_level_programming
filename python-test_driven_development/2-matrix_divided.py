bbe4d59c59819d576a04998bbe24d5bb615604b3
freddyrosa16 <freddy.rosa16@gmail.com>
2024-02-02 11:28:05 -0400

    1. Divide a matrix

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module contains a function that divide a matrix
+named matrix_divided that you pass 2 arguments
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    This function divide a matrix by the
+    number number passed to us
+
+    matrix = matrix to be divided
+    div = number to be divided
+    """
+    msg_error = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(matrix) is not list:
+        raise TypeError(msg_error)
+    if type(div) not in [int, float]:
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+
+    for row in matrix:
+        for value in matrix:
+            if type(value) not in [int, float]:
+                return TypeError(msg_error)
+    first_row = len(matrix[0])
+    for row in matrix:
+        if first_row != len(row):
+            raise TypeError('Each row of the matrix must have the same size')
+    return [[round(i / div, 2) for i in row] for row in matrix]
