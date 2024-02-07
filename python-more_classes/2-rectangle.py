fbb02eb76e44df77d7dfc6ac8cbcf525a4811e25
freddyrosa16 <freddy.rosa16@gmail.com>
2024-02-07 18:06:28 -0400

    2. Area and Perimeter

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-''''''
+"""
+This module have a class
+that defines a rectangle
+"""
 
 
 class Rectangle:
+    """ Definition of rectangle attribute """
+
     def __init__(self, width=0, height=0):
         self.__height = height
         if type(height) is not int:
@@ -17,24 +22,38 @@ class Rectangle:
 
     @property
     def height(self):
+        """ Return the height of the rectangle """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ Sets the height of the rectangle """
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
         self.__height = value
 
     @property
     def width(self):
+        """ Return the width of the rectangle """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ Sets the width of the rectangle """
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     def area(self):
+        """ Looks for the area of the rectangle """
         return self.height * self.width
 
     def perimeter(self):
+        """ Looks for the perimeter of the rectangle """
         if self.width == 0 or self.height == 0:
             return 0
         else:
