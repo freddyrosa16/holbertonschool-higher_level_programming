90f2a942100a70370a715dd6d0b590b0035f5daf
freddyrosa16 <freddy.rosa16@gmail.com>
2024-02-08 13:45:40 -0400

    1. My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module contains a child class
+named MyList that inherit from list.
+"""
+
+
+class MyList(list):
+    """
+    This class inherits from list,
+    it contain also a function that print
+    a sorted list
+    """
+
+    def print_sorted(self):
+        """
+        This function prints a list sorted
+        in ascending order of integers
+        """
+        list_sorted = []
+        for num in self:
+            list_sorted.append(num)
+            list_sorted.sort
+            print(list_sorted)
