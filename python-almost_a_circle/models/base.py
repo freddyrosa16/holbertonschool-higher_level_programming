4498728cee28ab3ff6cc7827f7e34abb62a8d80d
freddyrosa16 <freddy.rosa16@gmail.com>
2024-02-13 18:07:27 -0400

    Fixing error on writing nb

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,5 +20,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base._nb_objects += 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
