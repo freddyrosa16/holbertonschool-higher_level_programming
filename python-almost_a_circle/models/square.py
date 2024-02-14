bb8cb47df0cee59a5096d1d96bf20a468c201a69
freddyrosa16 <freddy.rosa16@gmail.com>
2024-02-14 15:18:25 -0400

    Making square inherit rectangle

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contains the class Square
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square that inherit from Rectangle
+    it receive width, height, x, y and id from Base
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        When initialise she validates value and then
+        proceed to assign them.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return (f'[Square] ({self.id}) {self.x}/{self.y} - {self.size}')
+
+    def size(self):
+        return super().width
+
+    def size(self, size):
+        self.width = size
