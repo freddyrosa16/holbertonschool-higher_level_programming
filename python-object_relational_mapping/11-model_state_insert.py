17f41a81892b6da600bb9b2d7f73bcc2f02bed13
freddyrosa16 <freddy.rosa16@gmail.com>
2024-03-22 10:18:00 -0400

    11. Add a new state

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,29 @@
+
+#!/usr/bin/python3
+"""Lists first states object from the database"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    """Lists first state object from the database"""
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{user}:{password}@localhost:3306/{database}"
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_obj = State(name='Louisiana')
+    session.add(state_obj)
+    session.commit()
+    states = session.query(State).distinct().count()
+    print(states)
